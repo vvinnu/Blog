965e1729ff399cbf1ed1cea684343dc0dde615a7
vvinnu <Vineeth Kanoor@VINNI>
2025-04-02 20:51:59 -0400

    Adding basic setup code and the main.ejs file

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require ('express');
+const path = require ('path');
+const mongoose = require('mongoose');
+const {check,validationResult} = require('express-validator');
+
+var app=express();
+app.use(express.urlencoded({extended:false}));
+
+app.set('views',path.join(__dirname,'views'));
+app.use(express.static(__dirname + '/public'));
+app.set('view engine' , 'ejs');
+
+//Rendering the main home page
+
+app.get('/',(req,res)=>{
+    res.render('main');
+});
+
+app.listen(8000);
+console.log("Listening to port 8000");
