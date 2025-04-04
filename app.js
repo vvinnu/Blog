3afe156c8d1181d43bbd19f606950556d0cc6d61
vvinnu <Vineeth Kanoor@VINNI>
2025-04-04 01:59:40 -0400

    Added the login functionality for admin using MongoDB as a database to verify the credentials

--- app.js
+++ app.js
@@ -10,6 +10,49 @@ app.set('views',path.join(__dirname,'views'));
 app.use(express.static(__dirname + '/public'));
 app.set('view engine' , 'ejs');
 
+
+//Connecting to MongoDB
+
+var admin = mongoose.model('users',{
+    username:String,
+    password:String
+})
+
+mongoose.connect('mongodb://localhost:27017/Blog');
+
+// Verifying the login credentials
+app.post('/login',[
+    check('username','Username Cannot be Empty').notEmpty(),
+    check('password','Password Cannot be Empty').notEmpty()
+],(req,res)=>{
+    var errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.render('login',{formErrors:errors.array()});
+    }
+    var username = req.body.username;
+    var password = req.body.password;
+    
+    admin.findOne({username:username}).then((data)=>{
+        if(data == null){
+            console.log("Invalid Username");
+            return res.render('login',{
+                errorMessage:"Invalid Username"
+            });
+        }else {
+            if(data.password == password){
+            
+            console.log("Invalid Password");
+            return res.render('login',{
+                errorMessage:"Invalid Password"
+            });
+        }else{
+            console.log("Login Success");
+            return res.redirect('/admin');
+        }
+        }
+    });
+});
+
 //Rendering the main home page
 
 app.get('/',(req,res)=>{
